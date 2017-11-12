d3829863c475fec62f469ba5e1a59109340356ba
William Crump <ricky@Williams-MacBook-Pro.local>
2017-11-11 22:13:10 -0500

    Dropoff has been implemented.

--- src/magicschoolbus/driver.clj
+++ src/magicschoolbus/driver.clj
@@ -1,6 +1,8 @@
 (ns magicschoolbus.driver
   (:import java.io.File)
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [magicschoolbus.school-bus :as bus]))
 
 (defn match?
   "Determines if `regex` matches `string` input"
@@ -13,6 +15,8 @@
 (defn file-name-matches? [regex filename]
   (match? regex (.getName (io/file filename))))
 
+(def filename-regex #"^\\\\(.+\\\\)*(.+)\\.(.+)$\n")
+
 (defn pickup
   "Read all files in `directory` that matches `regex`.
   `directory` and `regex` are of type `string`"
@@ -22,3 +26,11 @@
        (filter #(.isFile %))
        (filter #(file-name-matches? regex %))
        (mapv #(.getAbsolutePath %))))
+
+(defn getName [file]
+  (.getName (io/file file)))
+
+(defn dropoff
+  "Dropping off `files` at `destination`."
+  [destination files]
+  (doseq [file files] bus/move file (str destination (getName file))))
--- test/magicschoolbus/driver_test.clj
+++ test/magicschoolbus/driver_test.clj
@@ -1,4 +1,5 @@
 (ns magicschoolbus.driver-test
+  (:import java.io.File)
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
             [magicschoolbus.driver :as driver]))
@@ -7,6 +8,8 @@
 (def orig-path (str orig-dir "/orig.txt"))
 (def orig-path-two (str orig-dir "/orig2.txt"))
 (def fake-path (str orig-dir "/fake.txt"))
+
+(def copy-dir "test/magicschoolbus/workspace/copy")
 
 (defn write-file []
   (spit orig-path "hello")
@@ -29,11 +32,19 @@
 
 (use-fixtures :each setup)
 
-(deftest driver-regex-matches-file-name
+(deftest driver-regex-matches-file-name-test
   (testing "Driver file name with matching regex returns true"
     (is (true? (driver/match? #"or.*" "orig.txt")))))
 
-(deftest driver-reads-all-matching-files-from-directory
+(deftest driver-reads-all-matching-files-from-directory-test
   (testing "Driver gets all the file names from a given directory"
     (is (pos? (compare [orig-path orig-path-two]
                        (driver/pickup orig-dir #"or.*"))))))
+
+(deftest driver-drops-off-files-test
+  (testing "Driver moves/deletes files after pickup"
+    (driver/dropoff copy-dir [orig-path orig-path-two])
+    (is (io/as-file (str copy-dir "/orig.txt")))
+    (is (io/as-file (str copy-dir "/orig2.txt")))
+    (is (false? (.exists (io/file (str copy-dir "/orig.txt")))))
+    (is (false? (.exists (io/file (str copy-dir "/orig2.txt")))))))
