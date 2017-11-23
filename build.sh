b3ca0ddf1f23405f336bc36efed5ce3348df8a63
William Crump <ricky@Williams-MacBook-Pro.local>
2017-11-22 19:00:31 -0500

    Changed build script to create a root magicschoolbus directory before taring. Added example launchd config file.

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 lein uberjar
-cp ./config.json ./target/uberjar/
-mkdir ./target/uberjar/logs
-cd ./target/uberjar && tar -czvf magicschoolbus.tar.gz magicschoo*stand* config.json logs
+
+mkdir ./target/uberjar/magicschoolbus
+mkdir ./target/uberjar/magicschoolbus/logs
+
+cp ./target/uberjar/magicschoo*stand* ./target/uberjar/magicschoolbus
+cp ./config.json ./target/uberjar/magicschoolbus
+
+cd ./target/uberjar && tar -czvf magicschoolbus.tar.gz magicschoolbus
